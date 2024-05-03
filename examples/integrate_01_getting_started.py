b48a2257120a04e0633974e707d085de6109fa23
thomas mejer hansen <thomas.mejer.hansen@gmail.com>
2024-05-03 14:51:29 +0200

    Added WorkBench type prior: ig.prior_model_workbench

--- examples/integrate_01_getting_started.py
+++ examples/integrate_01_getting_started.py
@@ -44,15 +44,15 @@ print("Using GEX file: %s" % file_gex)
 
 # %% A. CONSTRUCT PRIOR MODEL OR USE EXISTING
 #N=5000000
-N=10000
+N=100000
 # Layered model
 f_prior_h5 = ig.prior_model_layered(N=N,lay_dist='chi2', NLAY_deg=5)
 # WorkBench type layered model
-f_prior_h5 = ig.prior_model_workbench(rho_dist='chi2', nlayers=5)
-f_prior_h5 = ig.prior_model_workbench(rho_dist='normal', nlayers=5)
-f_prior_h5 = ig.prior_model_workbench(rho_dist='uniform', nlayers=5)
-f_prior_h5 = ig.prior_model_workbench(rho_dist='log-normal', nlayers=5)
-f_prior_h5 = ig.prior_model_workbench(rho_dist='log-uniform', nlayers=11, rho_min = 10, rho_max = 300)
+f_prior_h5 = ig.prior_model_workbench(N=N,rho_dist='chi2', nlayers=5)
+f_prior_h5 = ig.prior_model_workbench(N=N,rho_dist='normal', nlayers=5)
+f_prior_h5 = ig.prior_model_workbench(N=N,rho_dist='uniform', nlayers=5)
+f_prior_h5 = ig.prior_model_workbench(N=N,rho_dist='log-normal', nlayers=5)
+f_prior_h5 = ig.prior_model_workbench(N=N,rho_dist='log-uniform', nlayers=11, rho_min = 10, rho_max = 300)
 
 #f_prior_h5 = 'PRIOR_Daugaard_N100000.h5'
 
