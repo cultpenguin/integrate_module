cd95f9fdbb30a131c516bc663d4a2c09d08f3253
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2024-09-21 21:58:37 +0200

    Install GA-AEM with homebrew on OSX

---
+++ scripts/cmake_build_script_homebrew_gatdaem1d.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# This script is for compiling the forward code, gatdaem1d, from ga-aem, on Ubuntu (version >= 2.0.0)
+# See more detailed information on https://github.com/GeoscienceAustralia/ga-aem/blob/master/cmake_build_script_ubuntu.sh 
+
+# # Install necessary packages in Homebrew (tested on macOS XXX)
+brew update
+brew install gcc
+brew install fftw
+brew install open-mpi
+brew install cmake
+
+## 1. Clone the ga-aem repository from Github
+git clone --recursive https://github.com/GeoscienceAustralia/ga-aem.git
+#git clone https://github.com/GeoscienceAustralia/ga-aem.git
+cd ga-aem
+
+## 2. Compile GA-AEM
+
+# INSTALL_DIR is the directory for installing the build package
+export INSTALL_DIR=$PWD/install-ubuntu
+
+# BUILD_DIR is a temporary directory for building (compiling and linking)
+export BUILD_DIR=$PWD/build-ubuntu
+
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+# compile gatdaem1d
+cmake -Wno-dev -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DWITH_MPI=OFF -DWITH_NETCDF=OFF -DWITH_GDAL=OFF -DWITH_PETSC=OFF ..
+#cmake --build . --target gatdaem1d-static
+#cmake --build . --target gatdaem1d-shared
+cmake --build . --target matlab-bindings --config=Release
+cmake --build . --target python-bindings --config=Release
+cmake --install . --prefix $INSTALL_DIR
+
+## 3. Install python module 
+echo  $INSTALL_DIR
+# cd  $INSTALL_DIR
+
+## CHECK FOR shared library dependencies
+cd ..
+readelf -d install-ubuntu/python/gatdaem1d/gatdaem1d.so  | grep 'Shared'
+
+## Install the Python module
+echo "# Finally, install the Python module using"
+echo "cd ga-aem/install-ubuntu/python"
+echo "pip install ."
